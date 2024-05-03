c4f6d55549b110ad735b98bbb3332f75623c0de6
Asahi Takenouchi <asahi.taken@gmail.com>
2024-05-03 17:02:23 +0900

    chore: exclude wc and uniq from list of generating test files on ci.sh

--- 06_uniqr/ci.sh
+++ 06_uniqr/ci.sh
@@ -3,4 +3,9 @@
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 cd ${SCRIPT_DIR}
 
-./mk-outs.sh
+# Linux version `uniq` is very different from macOS version
+# Run this script on macOS machine and then commit the generated files
+
+if [ "$(uname)" == 'Darwin' ]; then
+  ./mk-outs.sh
+fi
